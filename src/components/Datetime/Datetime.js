6e508b02cef11c01804e02eca06919c4823363f4
Israel <israel4sng@yahoo.com>
2021-02-16 17:54:44 +0800

    update Datetime component

--- src/components/Datetime/Datetime.js
+++ src/components/Datetime/Datetime.js
@@ -3,7 +3,7 @@ import "./Datetime.css";
 export default function Datetime({ date, time }) {
   return (
     <div className="datetime">
-      <time datetime={`${date} ${time}`}>
+      <time dateTime={`${date} ${time}`}>
         <span>{date}</span>
         <span>{time}</span>
       </time>
